5f2fe03fd9ecf3d50961f72e07a31b889e6684af
riyaz <riyaz.shaikit@gmail.com>
2026-06-21 19:53:18 +0530

    fix: move firebase credentials to env vars

--- frontend/context/firebase.tsx
+++ frontend/context/firebase.tsx
@@ -1,20 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCwLwoppbsA4wWZVTh_s1XorATq6Nu9wpQ",
-  authDomain: "twitter-45e7e.firebaseapp.com",
-  projectId: "twitter-45e7e",
-  storageBucket: "twitter-45e7e.firebasestorage.app",
-  messagingSenderId: "620085951064",
-  appId: "1:620085951064:web:05d7644d7afdd608e6682d"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth=getAuth(app)
-export default app
+export const auth = getAuth(app);
+export default app;
